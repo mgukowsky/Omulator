d90901179477a743762466bc83b37dbc353a9666
Matt Gukowsky <mgukowsky@gmail.com>
2021-05-08 00:44:56 -0400

    Update dockerbuild image name (oml-builder -> mgukowsky/oml-builder)

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,6 +2,6 @@
 # Simple driver to perform a docker build
 set -eu
 
-docker run -it --user `id -u`:`id -g` --rm -v `pwd`:/omulator oml-builder $@
+docker run -it --user `id -u`:`id -g` --rm -v `pwd`:/omulator mgukowsky/oml-builder $@
 # Fix paths in compile_commands.json now that we're out of the container
 sed -i -re "s/\/omulator/$(pwd | sed -re 's/\//\\\//g')/g" compile_commands.json
