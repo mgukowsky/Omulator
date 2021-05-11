c40852b565b3cdbacfdc148eb48b3962ea16e9b5
Matt Gukowsky <mgukowsky@gmail.com>
2021-05-11 02:40:40 -0400

    Add missing CMAKE_AR executable to msvc-wsl toolchain, and add explanatory comment to that toolchain file

--- cmake/Toolchain-msvc-wsl.cmake
+++ cmake/Toolchain-msvc-wsl.cmake
@@ -1,8 +1,13 @@
+# Toolchain that allows MSVC to be used within WSL. Assumes that all of these .exe files can
+# be found in $PATH. The easiest way to do this is to run the 'vcvars64.bat' script that ships with
+# Visual Studio in a shell/shortcut prior to launching wsl.exe in order to set all of the required
+# variables.
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR "AMD64")
 
 set(CMAKE_C_COMPILER cl.exe)
 set(CMAKE_CXX_COMPILER cl.exe)
 set(CMAKE_LINKER link.exe)
+set(CMAKE_AR lib.exe)
 set(CMAKE_MT mt.exe)
 set(CMAKE_RC rc.exe)
