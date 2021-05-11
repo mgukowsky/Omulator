d7414d8d5793c31e4c3f5124455b55acc4029bef
Matt Gukowsky <mgukowsky@gmail.com>
2021-05-10 23:54:14 -0400

    Have dockerbuild.sh check for compile_commands.json before attempting to alter it.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,6 +2,10 @@
 # Simple driver to perform a docker build
 set -eu
 
+COMPILE_COMMANDS=compile_commands.json
+
 docker run -it --user `id -u`:`id -g` --rm -v `pwd`:/omulator mgukowsky/oml-builder $@
-# Fix paths in compile_commands.json now that we're out of the container
-sed -i -re "s/\/omulator/$(pwd | sed -re 's/\//\\\//g')/g" compile_commands.json
+if [[ -e $COMPILE_COMMANDS ]]; then
+  # Fix paths in compile_commands.json now that we're out of the container
+  sed -i -re "s/\/omulator/$(pwd | sed -re 's/\//\\\//g')/g" $COMPILE_COMMANDS
+fi
