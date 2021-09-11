f53cb5dfba699ae9610db1758bde04a42d822a42
Matt Gukowsky <mgukowsky@gmail.com>
2021-09-11 19:45:06 -0400

    Mark omulator::util::reinterpret() as OML_FORCEINLINE

--- include/omulator/util/reinterpret.hpp
+++ include/omulator/util/reinterpret.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "omulator/oml_defines.hpp"
+
 namespace omulator::util {
 /**
  * Given that we're inherently dealing with raw-memory manipulation throughtout this project, this
  * function gives us some syntactic sugar over the common reinterpret_cast pattern.
  */
 template<typename To_t, typename From_t>
-To_t &reinterpret(From_t *t) noexcept {
+OML_FORCEINLINE To_t &reinterpret(From_t *t) noexcept {
   return *(reinterpret_cast<To_t *>(t));
 }
 }  // namespace omulator::util
